19dbf843a9f9e2ecc152ac377c3a4a8d1c5891e0
Ricardo Ugalde <ugalde.rj@gmail.com>
2017-10-08 17:11:44 -0600

    Framework template

--- Main.py
+++ Main.py
@@ -6,6 +6,128 @@ import csv
 Funcionarios = []
 Clientes = []
 
+Agencias = []
+Aerolineas = []
+Aviones = []
+Rutas = []
+
+Trips = []
+Tripulacion = []
+
+Vuelos = []
+Destinos = []
+
+
+AsignacionVuelos = []
+TripulaVuela = []
+ClientesVuelo = []
+
+def eliminarRepetidos(lista,idIndex):
+
+	sinRepetidos = []
+	sinRepetidos.append(lista[0])
+	
+	
+	for item in lista:
+		estaRepetido = False
+		newItem = item[idIndex]
+		
+		for elem in sinRepetidos:
+			if elem[idIndex] == newItem:
+				estaRepetido = True
+		if not estaRepetido:
+			sinRepetidos.append(item)
+	return sinRepetidos
+        
+
+
+def limpiarListas():
+	
+	global Funcionarios
+	global Clientes
+
+	global Agencias
+	global Aerolineas
+	global Aviones
+	global Rutas
+
+	global Trips
+	global Tripulacion
+
+	global Vuelos
+	global Destinos
+
+
+	global AsignacionVuelos
+	global TripulaVuela 
+	global ClientesVuelo
+
+	Funcionarios = eliminarRepetidos(Funcionarios,0)
+	Clientes = eliminarRepetidos(Clientes,0)
+
+	Agencias = eliminarRepetidos(Agencias,0)
+	#Aerolineas = eliminarRepetidos(Aerolineas,1)
+	#Aviones = eliminarRepetidos(Aviones,2)
+	#Rutas = eliminarRepetidos(Rutas,3)
+
+	Trips = eliminarRepetidos(Trips,0)
+	#Tripulacion = eliminarRepetidos(Tripulacion,0)
+
+	#Vuelos = eliminarRepetidos(Vuelos,0)
+	Destinos = eliminarRepetidos(Destinos,0)
+
+
+	#AsignacionVuelos = eliminarRepetidos(AsignacionVuelos,0)
+	#TripulaVuela = eliminarRepetidos(TripulaVuela,0)
+	#ClientesVuelo = eliminarRepetidos(ClientesVuelo,0)
+
+
+
+def loadMemory():
+
+	global Funcionarios
+	global Clientes
+
+	global Agencias
+	global Aerolineas
+	global Aviones
+	global Rutas
+
+	global Trips
+	global Tripulacion
+
+	global Vuelos
+	global Destinos
+
+
+	global AsignacionVuelos
+	global TripulaVuela 
+	global ClientesVuelo
+
+
+	Funcionarios.extend(LeerArchivos("Funcionarios"))
+	Clientes.extend(LeerArchivos("Clientes"))
+
+	Agencias.extend(LeerArchivos("Agencias"))
+	Aerolineas.extend(LeerArchivos("Aerolineas"))
+	Aviones.extend(LeerArchivos("Aviones"))
+	Rutas.extend(LeerArchivos("Rutas"))
+
+	Trips.extend(LeerArchivos("Trip"))
+	Tripulacion.extend(LeerArchivos("Tripulacion"))
+
+	Vuelos.extend(LeerArchivos("Vuelos"))
+	Destinos.extend(LeerArchivos("Destinos"))
+
+
+	AsignacionVuelos.extend(LeerArchivos("Asignacion de Vuelos"))
+	TripulaVuela.extend(LeerArchivos("TripulaVuela"))
+	ClientesVuelo.extend(LeerArchivos("ClientesVuelo"))
+
+	
+	
+
+	
 
 
 
@@ -15,7 +137,7 @@ def LeerColumna(FilePath,column):
 		func_reader = csv.reader(csvfile, delimiter=';', quotechar='|') #Extrae las filas del csv con el separador indicado, en este caso ;
 		
 		for fila in func_reader:
-			print(fila)
+			
 			ListaSalida.append(fila[column]) # Guarda en la lista de salida los strings de la columna i
 	return ListaSalida
 
@@ -70,17 +192,29 @@ def  Main():
 	
 	
 	#else:
-		print("Es usted un:")
-		print("1) cliente") 
-		print("2) funcionario") 
+
+	loadMemory()
+	limpiarListas()
+	
+	
+	while True:
+		
+		print("Que desea hacer:")
+		print("1) Iniciar sesión como cliente") 
+		print("2) Iniciar sesión como funcionario")
+		print("3) Reportes")
+		print("S) Salir del sistema")
+		
 		respuesta=input(": ")
+		print("")
+		
 		if respuesta == "1":
 			while True:
 
 				nombreCliente = input("Digite su login, o presione S para salir: ")
 
 				if nombreCliente.lower()=="s":
-					print("Saliendo...")
+					
 					break
 				elif loginClientes(nombreCliente):
 					LoopClientes()
@@ -94,7 +228,7 @@ def  Main():
 
 				nombreFunc = input("Digite su login, o presione S para salir: ")       
 				if nombreFunc.lower()=="s":
-					print("Saliendo...")
+					
 					break
 				elif loginFuncionarios(nombreFunc):
 					LoopFuncionarios()
@@ -103,19 +237,131 @@ def  Main():
 					print("Usuario incorrecto. Ingrese nuevamente el usuario.")
 					print("")
 
+		if respuesta == "3":
+			loopReportes()
+		if respuesta == "s" or respuesta == "S":
+			break
 
 
+def LoopEliminar():
+	while True:
+		print("Que desea eliminar:")
+		print("1) Aviones")
+		print("2) Rutas")
+		print("3) Vuelos")		
+		print("4) Clientes")
+		print("5) Asignación de vuelos")
+		print("S) Salir")
+
+		respuesta=input(": ")
+		print("")
+		
+		if respuesta == "1":
+
+			print("Aviones que se pueden eliminar:")
+			for i in Aviones:
+				print(i[1]+" | "+i[2])
+			
+			aerolinea = input("Digite la aerolinea: ")
+			avion = input("Digite la placa: ")
+			for item in Aviones[:]:
+				if item[1] == aerolinea and item[2] == avion:
+					Aviones.remove(item)
+					print("Avion eliminado.")
+					print("")
+				
+		if respuesta == "2":
+			pass
+		if respuesta == "3":
+			pass
+		if respuesta == "4":
+			pass
+		if respuesta == "5":
+			pass
+		if respuesta == "S":
+			break
 
 def LoopFuncionarios():
-	ListaFuncionarios=LeerArchivos("Funcionarios")
 	while True:
-		pass
+		
+		print("Que desea consultar:")
+		print("1) Eliminar aviones, rutas, vuelos, clientes o asignacion de vuelos")
+		print("2) Aerolíneas de una agencia")
+		print("3) Vuelos de una Aerolínea")		
+		print("4) Rutas de un vuelo")
+		print("5) Asignación de vuelos")
+		print("6) Puestos disponibles")
+		print("7) Puesto de un funcionario")
+		print("8) Horas de una ruta")
+		print("9) Rango de un funcionario")
+		print("S) Salir")
+		
+		respuesta=input(": ")
+		print("")
+		
+		if respuesta == "1":
+			
+			LoopEliminar()
+			
+			
+		if respuesta == "2":
+			pass
+		if respuesta == "3":
+			pass
+		if respuesta == "4":
+			pass
+		if respuesta == "5":
+			pass
+		if respuesta == "6":
+			pass
+		if respuesta == "7":
+			pass
+		if respuesta == "8":
+			pass
+		if respuesta == "9":
+			pass
+		if respuesta == "S":
+			break
 	
 
 def LoopClientes():
 	while True:
-		pass
-	pass
+		
+		print("Que desea consultar:")
+		print("1) Agencias de viajes")
+		print("2) Aerolíneas de una agencia")
+		print("3) Vuelos de una Aerolínea")		
+		print("4) Rutas de un vuelo")
+		print("5) Asignación de vuelos")
+		print("6) Puestos disponibles")
+		print("7) Puesto de un funcionario")
+		print("8) Horas de una ruta")
+		print("9) Rango de un funcionario")
+		print("S) Salir")
+		
+		respuesta=input(": ")
+		print("")
+		
+		if respuesta == "1":
+			pass
+		if respuesta == "2":
+			pass
+		if respuesta == "3":
+			pass
+		if respuesta == "4":
+			pass
+		if respuesta == "5":
+			pass
+		if respuesta == "6":
+			pass
+		if respuesta == "7":
+			pass
+		if respuesta == "8":
+			pass
+		if respuesta == "9":
+			pass
+		if respuesta == "S":
+			break
 
 
 		
